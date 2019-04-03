21a2a543f6983396932522291033bbb3b2ab42b1
muzamirus <sebulimekeith007@gmail.com>
2019-04-03 19:02:12 +0300

    pushing files to github

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,30 +8,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 Button btncreate, login;
+TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btncreate=(Button) findViewById(R.id.create_account);
+        btncreate = (Button) findViewById(R.id.create_account);
         btncreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent obj= new Intent(MainActivity.this,Account.class);
+                Intent obj = new Intent(MainActivity.this, Account.class);
                 startActivity(obj);
             }
         });
-        login=(Button) findViewById(R.id.login);
+        txt = (TextView) findViewById(R.id.forgot);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent obj = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(obj);
+            }
+        });
+        login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent obj= new Intent(MainActivity.this,Main2Activity.class);
+                Intent obj = new Intent(MainActivity.this, Testfrag.class);
                 startActivity(obj);
             }
         });
-    }
-}
-
+    }}
