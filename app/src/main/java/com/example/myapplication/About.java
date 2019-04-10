a49d017bbee83abea64207413479e1d2aaa9c99d
muzamirus <sebulimekeith007@gmail.com>
2019-04-10 08:07:59 +0300

    pushing files to github

--- app/src/main/java/com/example/myapplication/About.java
+++ app/src/main/java/com/example/myapplication/About.java
@@ -1,30 +1,59 @@
 package com.example.myapplication;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 
 import static com.example.myapplication.R.id.btn;
 
 public class About extends AppCompatActivity {
 ImageButton btnabout;
+CheckBox btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        btnabout= (ImageButton) findViewById(R.id.btn);
+
+
+        btnabout = (ImageButton) findViewById(R.id.btn);
         btnabout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent object= new Intent(About.this,MainActivity.class);
+                Intent object = new Intent(About.this, MainActivity.class);
                 startActivity(object);
+            }
+
+        });
+
+        btn = (CheckBox) findViewById(R.id.terms);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNotification();
             }
         });
     }
+            private void addNotification() {
+                NotificationCompat.Builder builder=new NotificationCompat.Builder(this)
+                        .setContentText("Welcome to the Party App!")
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle("PARTY APP");
+                Intent notification=new Intent(About.this,MainActivity.class);
+                PendingIntent intent=PendingIntent.getActivity(this,0,notification,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(intent);
+                NotificationManager manager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0,builder.build());
+    }
+
     protected void onStart(){
         super.onStart();
         Log.d("tag","In onStart");
